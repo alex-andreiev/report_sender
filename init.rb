ccb7b1d329cf50445646388c6e460149a2366a20
alexckua <alex@net.ck.ua>
2018-08-09 19:12:12 +0300

    Initial commit

---
+++ init.rb
@@ -0,0 +1,7 @@
+Redmine::Plugin.register :report_sender do
+  name 'Report Sender plugin'
+  author 'Alex Andreiev'
+  description 'This is a plugin for Redmine. The plugin are sending report via POST request to remote server when user modify task.'
+  version '0.0.1'
+  author_url 'https://github.com/alexckua'
+end
